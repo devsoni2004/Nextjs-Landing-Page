27ef3aa013506b734e582a16486336ccb44ec9e4
devsoni2004 <vipbrothers92@gmail.com>
2023-07-05 00:02:14 +0530

    Navbar Components Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a65582ea257efe2"; }
+/******/ 		__webpack_require__.h = function() { return "43f5ad21e5d24597"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Navbar from "./Components/Navbar"
-import Footer  from "./Components/Footer"
+import Navbar from "./components/Navbar"
+import Footer  from "./components/Footer"
 import Head from "next/head";
 import Link from 'next/link';
 
